986aaf6f28e3c00be064862b23e2e1180e6eb4c4
Aboubacar Ali <amsaboubacar06@gmail.com>
2025-03-19 05:37:46 +0100

    fix in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/mon-portfolio/", // ⚠️ Mets le NOM EXACT de ton dépôt GitHub ici
+  plugins: [react(), tailwindcss()],
+  base: "/mon-portfolio/",
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import tailwindcss from "@tailwindcss/vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: "/mon-portfolio/",
-// });
